7e598f878f1d25c59082ad768d79ff630538da4c
Napoleon-Christos Oikonomou <Napoleonoikon@gmail.com>
2017-11-10 17:07:24 +0200

    removed legacy files

--- app/dev/initDB.js
+++ app/dev/initDB.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/callbymeaning';
 
-// This scripts creates a new db with everything from the default one. mongod MUST be already running before used.
+/*
+ * This script creates a new db with everything from the default one. mongod MUST be already running before used.
+ * Run this before starting the server.
+ */
 MongoClient.connect(url, (err, db) => {
   if (err) console.error(err);
   let mongoCommand = {copydb: 1, fromhost: 'localhost', fromdb: 'callbymeaning', todb: process.env.DB};
